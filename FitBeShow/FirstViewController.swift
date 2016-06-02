c01793f149101bd40ebf80c0afa01c1b469bc3eb
TimothyYeou <timyeou1234@hotmail.com>
2016-06-02 13:42:46 +0800

    Initial Commit

---
+++ FitBeShow/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FitBeShow
+//
+//  Created by YeouTimothy on 2016/6/2.
+//  Copyright © 2016年 YeouTimothy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FitBeShow/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FitBeShow
+//
+//  Created by YeouTimothy on 2016/6/2.
+//  Copyright © 2016年 YeouTimothy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
